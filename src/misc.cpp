a134f6c8a71b21adb126103d9ddb488a57f644e8
Marco Zerbinati <zerbinati.marco@gmail.com>
2021-03-01 15:35:04 +0100

    DeV version+new_NET_UP: Introduce DistanceFromPV
    We introduce a metric for each internal node in search, called
    DistanceFromPV.
    This distance indicated how far the current node is from the principal
    variation.
    
    We then use this distance to search the nodes which are close to the PV
    a little
    deeper (up to 4 plies deeper than the PV): this improves the quality of
    the search
    at these nodes and bring better updates for the PV during search.
    
    STC:
    LLR: 2.96 (-2.94,2.94) {-0.25,1.25}
    Total: 54936 W: 5047 L: 4850 D: 45039
    Ptnml(0-2): 183, 3907, 19075, 4136, 167
    https://tests.stockfishchess.org/tests/view/6037b88e7f517a561bc4a392
    
    LTC:
    LLR: 2.95 (-2.94,2.94) {0.25,1.25}
    Total: 49608 W: 1880 L: 1703 D: 46025
    Ptnml(0-2): 22, 1514, 21555, 1691, 22
    https://tests.stockfishchess.org/tests/view/6038271b7f517a561bc4a3cb
    
    Closes #3369
    
    Bench: 5037279
    master
    @snicolet
    @vondele
    snicolet authored

--- src/misc.cpp
+++ src/misc.cpp
@@ -81,7 +81,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-const string Version = "";
+const string Version = " ";
 
 /// Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 /// cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
@@ -244,7 +244,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "SugaR AI 1.50 " << Version << setfill('0');
+  ss << "SugaR AI " << Version << setfill('0');
 
   ss << (to_uci  ? "\nid author ": " by ")
      << "Stockfish Team, Marco Zerbinati, Khalid Omar";
