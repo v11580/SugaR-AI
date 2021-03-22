e85d480d5b98e07c59eaf114cc2d676c67efadcb
Marco Zerbinati <zerbinati.marco@gmail.com>
2021-03-22 15:54:26 +0100

    SugaR AI 1.70

--- src/misc.cpp
+++ src/misc.cpp
@@ -245,7 +245,7 @@ string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "SugaR AI 1.60 " << Version << setfill('0');
+  ss << "SugaR AI 1.70 " << Version << setfill('0');
 
   ss << (to_uci  ? "\nid author ": " by ")
      << "Stockfish Team, Marco Zerbinati, Khalid Omar";
